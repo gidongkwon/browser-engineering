0b30854008596a02ac5afed302b1d14af5e06634
Gidong Kwon <lucidfext@gmail.com>
2025-10-25 22:12:43 +0900

    Exercise 1-4

--- browser.py
+++ browser.py
@@ -1,15 +1,37 @@
 from url import URL
 
+entities = {"&lt;": "<", "&gt;": ">"}
+
+
+def lookahead(string: str, index: int, count: int):
+    string_length = len(string)
+    assert index >= 0 and index < string_length
+    assert count > 0
+
+    end = min(index + count, string_length)
+    return string[index:end]
+
 
 def show(body: str):
     in_tag = False
-    for c in body:
-        if c == "<":
+    index = 0
+    while index < len(body):
+        char = body[index]
+        if char == "&":
+            # hack wow
+            maybe_entity = lookahead(body, index, 4)
+            if maybe_entity in entities.keys():
+                print(entities[maybe_entity], end="")
+                index += len(maybe_entity)
+                continue
+        if char == "<":
             in_tag = True
-        elif c == ">":
+        elif char == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
+            print(char, end="")
+        index += 1
+    print()
 
 
 def load(url: URL):
