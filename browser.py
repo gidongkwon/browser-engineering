36116138339e7c1866b9d734c955efe2f63e7b5c
Gidong Kwon <lucidfext@gmail.com>
2025-10-29 12:57:03 +0900

    Exercise 2-4: Scrollbar

--- browser.py
+++ browser.py
@@ -8,17 +8,15 @@ import platform
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
+SCROLLBAR_WIDTH = 10
+
 
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(
-            self.window,
-            width=WIDTH,
-            height=HEIGHT
-        )
-        self.width = WIDTH
-        self.height = HEIGHT
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.document_width = WIDTH
+        self.document_height = HEIGHT
 
         self.canvas.pack(fill=BOTH, expand=True)
         self.scroll_y = 0
@@ -33,13 +31,11 @@ class Browser:
 
         # Resize event
         self.window.bind("<Configure>", self.handle_configure)
-        
 
     def load(self, url: URL):
         body = url.request()
         self.text = lex(body)
         self.layout(self.text)
-
 
         self.font = tkinter.font.Font(family="Noto Sans KR")
         self.draw()
@@ -47,15 +43,42 @@ class Browser:
     def draw(self):
         self.canvas.delete("all")
         for x, y, c in self.display_list:
-            if y > self.scroll_y + self.height:
+            if y > self.scroll_y + self.document_height:
                 continue
             if y + VSTEP < self.scroll_y:
                 continue
             self.canvas.create_text(x, y - self.scroll_y, text=c, font=self.font)
-    
+
+        # Scrollbar
+        if self.needs_scrollbar():
+            # Background
+            bar_x = self.document_width
+            self.canvas.create_rectangle(
+                bar_x,
+                0,
+                bar_x + SCROLLBAR_WIDTH,
+                self.document_height,
+                fill="#aaaaaa",
+                outline="",
+            )
+            # Bar
+            scroll_bottom = self.scroll_y_range[1]
+            bar_y = self.scroll_y / scroll_bottom * self.document_height
+            bar_height = self.document_height**2 / scroll_bottom
+            self.canvas.create_rectangle(
+                bar_x,
+                bar_y,
+                bar_x + SCROLLBAR_WIDTH,
+                bar_y + bar_height,
+                fill="blue",
+            )
+
     def scroll(self, delta=SCROLL_STEP, multiplier=1):
-        self.scroll_y += delta * multiplier 
-        self.scroll_y = max(self.scroll_y_range[0], min(self.scroll_y, self.scroll_y_range[1] - self.height))
+        self.scroll_y += delta * multiplier
+        self.scroll_y = max(
+            self.scroll_y_range[0],
+            min(self.scroll_y, self.scroll_y_range[1] - self.document_height),
+        )
         self.draw()
 
     def handle_wheel(self, e: tkinter.Event):
@@ -77,14 +100,16 @@ class Browser:
 
     def handle_linux_scrollup(self, e):
         self.scroll(multiplier=-1)
-    
+
     def handle_linux_scrolldown(self, e):
         self.scroll(multiplier=1)
-    
+
     def handle_configure(self, e: tkinter.Event):
-        self.width, self.height = e.width, e.height
+        self.document_width, self.document_height = e.width, e.height
+        if self.needs_scrollbar():
+            self.document_width -= SCROLLBAR_WIDTH
         self.layout(self.text)
-        self.scroll(delta=0) # redraw + scroll clipping
+        self.scroll(delta=0)  # redraw + scroll clipping
 
     def layout(self, text: str):
         self.display_list: list[tuple[int, int, str]] = []
@@ -93,16 +118,19 @@ class Browser:
             if c == "\n":
                 cursor_y += int(VSTEP * 1.2)
                 cursor_x = HSTEP
-            
+
             self.display_list.append((cursor_x, cursor_y, c))
             cursor_x += HSTEP
 
-            if cursor_x >= self.width - HSTEP:
+            if cursor_x >= self.document_width - HSTEP:
                 cursor_y += VSTEP
                 cursor_x = HSTEP
 
         # top to top
         self.scroll_y_range = (0, self.display_list[-1][1])
+
+    def needs_scrollbar(self):
+        return self.scroll_y_range[1] > self.document_height
 
 
 def lookahead(string: str, index: int, count: int):
@@ -135,6 +163,7 @@ def lex(body: str):
             text += char
         index += 1
     return text
+
 
 def main():
     import os
